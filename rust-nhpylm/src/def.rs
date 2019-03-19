82d44d0438859ec5a600ee6c1b13a948b51f9103
Xiang Ji <hi@xiangji.me>
2019-03-19 20:47:08 +0100

    Add constant definitions

--- rust-nhpylm/src/def.rs
+++ rust-nhpylm/src/def.rs
@@ -2,3 +2,16 @@ use std::hash::{Hash, Hasher};
 
 pub const BOS_CHAR: char = 'Α';
 pub const EOS_CHAR: char = 'Ω';
+pub const HPYLM_INITIAL_D: f64 = 0.5;
+pub const HPYLM_INITIAL_THETA: f64 = 2.0;
+pub const BOW: char = 'α';
+pub const EOW: char = 'ω';
+pub const HPYLM_INITIAL_d: f64 = 0.5;
+pub const HPYLM_INITIAL_theta: f64 = 2.0;
+pub const HPYLM_a: f64 = 1.0;
+pub const HPYLM_b: f64 = 1.0;
+pub const HPYLM_alpha: f64 = 1.0;
+pub const HPYLM_beta: f64 = 1.0;
+pub const CHPYLM_beta_STOP: f64 = 0.57;
+pub const CHPYLM_beta_PASS: f64 = 0.85;
+pub const CHPYLM_epsilon: f64 = 1e-12;
